5a2acb0b19c1c5598c580630d58b46f6843dd5a7
Roberto <Roberto@192.168.2.11>
2018-06-22 12:09:23 -0600

    Listo

---
+++ src/lag_planillas/LAG_Planillas.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lag_planillas;
+
+/**
+ *
+ * @author Gabriel Salinas
+ * 
+ */
+public class LAG_Planillas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   frmSeguridad objeto1=new frmSeguridad();
+   objeto1.setVisible(true);
+        
+        
+    }
+    
+}
